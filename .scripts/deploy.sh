3b4e216d33b6c096b890db0691b30c6a550e73d3
shobuj <shobujsa93@gmail.com>
2026-03-15 15:45:07 +0600

    udpate deploy.sh file

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -3,23 +3,35 @@ set -e
 
 echo "Deployment started ..."
 
-# Enter maintenance mode or return true
-# if already is in maintenance mode
-php artisan down  || true
+cd /home1/micro1
 
-# Pull the latest version of the app
+# Enter maintenance mode
+php artisan down || true
+
+echo "Pull latest code"
 git pull origin main --ff
 
-# Install composer dependencies
-# composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+echo "Install PHP dependencies"
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 
+echo "Install Node dependencies"
+npm install
 
-# Exit maintenance mode
-php artisan up
+echo "Build Vite assets"
+npm run build
 
-# Clear the old cache
-php artisan cache:clear
-php artisan clear-compiled
+echo "Run database migrations"
+php artisan migrate --force
+
+echo "Clear old cache"
 php artisan optimize:clear
+
+echo "Cache configs for performance"
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+echo "Exit maintenance mode"
+php artisan up
 
 echo "Deployment finished!"
